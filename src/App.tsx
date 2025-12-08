bf2a7ad3812f7a519e8cb590e055a67cfff97188
김한성 <hsung0714@gmail.com>
2025-12-08 23:06:34 +0900

    Design: 전체 디자인 수정
    -어두운 느낌이 나게 세련되게 수정

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import RecommendPage from './page/RecomendPage';
 function App() {
   return (
     <Router>
-      <div style={{ width: '100vw', height: '100vh', margin: 0, padding: 0, fontFamily: 'Arial, sans-serif' }}>
+      <div style={{ width: '100vw', minHeight: '100vh', margin: 0, padding: 0, fontFamily: 'Arial, sans-serif' }}>
         {/* 네비게이션이나 헤더가 필요하면 여기에 추가 */}
         <Routes>
           <Route path="/" element={<IntroducePage />} />
